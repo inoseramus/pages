875a1bf268f781a5d21dc95618fec747a2368343
seita <inoseramus@gmail.com>
2022-12-29 01:46:28 +0900

    a

---
+++ html/_sources/packages.rst.txt
@@ -0,0 +1,21 @@
+packages package
+================
+
+Submodules
+----------
+
+packages.my\_mod module
+-----------------------
+
+.. automodule:: packages.my_mod
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: packages
+   :members:
+   :undoc-members:
+   :show-inheritance:
---
+++ src/packages/my_mod.py
@@ -0,0 +1,20 @@
+class MyClass():
+
+    def __init__(self) -> None:
+        pass
+
+    def add(a:float, b:float)->float:
+        """This is a method
+        
+        .. math::
+            a + b = c
+
+        Args:
+            a (float): _description_
+            b (float): _description_
+
+        Returns:
+            float: _description_
+        """
+
+        return a+b
